cae011380ba9e00c4b83270652e689b1d7a13a2d
Quoding <quoding@protonmail.ch>
2020-01-28 11:21:35 -0500

    Change permission bits on logic_webapp.service to prevent puppet from giving warnings

--- webapp/install.sh
+++ webapp/install.sh
@@ -6,4 +6,4 @@ mkdir /var/www/logic_webapp/pdf
 mkdir /var/www/logic_webapp/plots
 mkdir /var/www/logic_webapp/pies
 
-install logic_webapp.service /etc/systemd/system/logic_webapp.service
+install -m 644 logic_webapp.service /etc/systemd/system/logic_webapp.service
