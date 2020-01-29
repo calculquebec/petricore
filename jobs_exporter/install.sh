89b54bafe62c732165466636ff8319ea48368f23
Quoding <quoding@protonmail.ch>
2020-01-29 11:54:29 -0500

    Change permission bits on jobs_exporter.service to prevent puppet from giving warnings

--- jobs_exporter/install.sh
+++ jobs_exporter/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-install jobs_exporter.service /etc/systemd/system/jobs_exporter.service
+install -m 644 jobs_exporter.service /etc/systemd/system/jobs_exporter.service
 install jobs_exporter.py /usr/sbin/jobs_exporter
