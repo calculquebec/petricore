fc17a632e63fd9c6e7a0a27ace8b242400e7a796
Quoding <quoding@protonmail.ch>
2020-01-29 09:24:18 -0500

    Add FLUSH PRIVILEGES; to create_user_job_view.sh

--- mgmt/create_user_job_view.sh
+++ mgmt/create_user_job_view.sh
@@ -11,6 +11,6 @@ domain=${ADDR[2]} #Find the specific domain name (i.e. kappa from mgmt01.int.kap
 
 loginhost="login1.int.${domain}.calculquebec.cloud"
 
-commands="CREATE USER '${newUser}'@'${loginhost}' IDENTIFIED BY '${newDbPassword}';USE slurm_acct_db; CREATE VIEW user_job_view AS SELECT id_user, id_job, job_name FROM ${domain}_job_table; GRANT SELECT ON user_job_view TO '${newUser}'@'${loginhost}';"
+commands="CREATE USER '${newUser}'@'${loginhost}' IDENTIFIED BY '${newDbPassword}';USE slurm_acct_db; CREATE VIEW user_job_view AS SELECT id_user, id_job, job_name FROM ${domain}_job_table; GRANT SELECT ON user_job_view TO '${newUser}'@'${loginhost}'; FLUSH PRIVILEGES;"
 
 echo "${commands}" | /usr/bin/mysql
