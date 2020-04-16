b474827efa826448e7114683a0348a0427c20a2d
Quoding <quoding@protonmail.ch>
2020-04-16 11:29:36 -0400

    Add nvidia_smi_exporter as a service for gpu metric export

--- jobs_exporter/install.sh
+++ jobs_exporter/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 install -m 644 jobs_exporter.service /etc/systemd/system/jobs_exporter.service
+install -m 644 nvidia_smi_exporter.service /etc/systemd/system/nvidia_smi_exporter.service
 install jobs_exporter.py /usr/sbin/jobs_exporter
